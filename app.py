d528e8b404352311acd79774ab41bd2fdfc6744a
Joshua <joshua032190@gmail.com>
2022-07-28 17:00:48 +0800

    empty flask... still need some changes

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
-app = Flask(__name__)
+class VueFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(variable_end_string = "%%",
+                              variable_start_string = "%%"))
 
+app = VueFlask(__name__)
+CORS(app,resources={r"/*":{"origins":"*"}})
 @app.route("/")
 def index():
     return render_template("index.html")
